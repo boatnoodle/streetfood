7473397bffdee8901bfede8ec72dfbfcd7c452a5
boatnoodle <nattasit31@gmail.com>
2020-03-31 21:45:12 +0700

    🤡Add database mockup

---
+++ src/dbMockup/index.tsx
@@ -0,0 +1,156 @@
+export const providers = [
+  {
+    id: 1,
+    name: "ร้านโกเอ"
+  },
+  {
+    id: 2,
+    name: "FOOD PANDA"
+  },
+  {
+    id: 3,
+    name: "GRAB"
+  },
+  {
+    id: 4,
+    name: "LINE MAN"
+  }
+];
+
+export const typeMenus = [
+  {
+    id: 1,
+    name: "ก๋วยเตี๋ยว"
+  },
+  {
+    id: 2,
+    name: "ข้าว"
+  },
+  {
+    id: 3,
+    name: "ลวกจิ้ม"
+  }
+];
+
+export const typeNoodles = [
+  {
+    name: "เรือ"
+  },
+  {
+    name: "น้ำตก"
+  },
+  {
+    name: "ต้มยำหมูสับ"
+  }
+];
+
+export const noodles = [
+  {
+    name: "หมี่ขาว"
+  },
+  {
+    name: "เล็ก"
+  },
+  {
+    name: "ใหญ่"
+  },
+  {
+    name: "บะหมี่"
+  },
+  {
+    name: "วุ้นเส้น"
+  },
+  {
+    name: "มาม่า"
+  },
+  {
+    name: "เกาเหลา"
+  }
+];
+
+export const toppingPork = [
+  {
+    name: "ลูกชิ้นหมู",
+    price: 10
+  },
+  {
+    name: "หมูสด",
+    price: 10
+  },
+  {
+    name: "หมูตุ๋น",
+    price: 10
+  },
+  {
+    name: "ตับ",
+    price: 10
+  },
+  {
+    name: "หัวใจ",
+    price: 10
+  },
+  {
+    name: "ม้ามหมู",
+    price: 10
+  }
+];
+
+export const toppingBeef = [
+  {
+    name: "ลูกชิ้นเนื้อ",
+    price: 10
+  },
+  {
+    name: "เนื้อสด",
+    price: 10
+  },
+  {
+    name: "เนื้อตุ๋น",
+    price: 10
+  },
+  {
+    name: "ตับ",
+    price: 10
+  },
+  {
+    name: "ผ้าขี้ริ้ว",
+    price: 10
+  },
+  {
+    name: "ม้ามเนื้อ",
+    price: 10
+  }
+];
+
+export const presetRemarks = [
+  {
+    name: "ไม่กระเทียม"
+  },
+  {
+    name: "ไม่ผัก"
+  },
+  {
+    name: "ไม่งอก"
+  },
+  {
+    name: "ไม่ผักโรย"
+  },
+  {
+    name: "ไม่ผักบุ้ง"
+  },
+  {
+    name: "ไม่ชูรส"
+  },
+  {
+    name: "น้ำซุปเยอะๆ"
+  },
+  {
+    name: "น้ำซุปน้อยๆ"
+  },
+  {
+    name: "ตับหมก"
+  },
+  {
+    name: "ลวกไม่สุก"
+  }
+];
